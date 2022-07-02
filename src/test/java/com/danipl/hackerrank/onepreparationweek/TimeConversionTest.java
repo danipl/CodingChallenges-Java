03924e2a5bb49cadeb2981283711775c10eec649
danipl <daniplst@gmail.com>
2022-07-02 17:48:18 +0200

    Adds TimeConversion.

---
+++ src/test/java/com/danipl/hackerrank/onepreparationweek/TimeConversionTest.java
@@ -0,0 +1,20 @@
+package com.danipl.hackerrank.onepreparationweek;
+
+import org.junit.jupiter.api.Test;
+
+import static com.danipl.hackerrank.onepreparationweek.TimeConversion.timeConversion;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimeConversionTest {
+
+    @Test
+    public void testSimple(){
+        assertEquals("12:01:00", timeConversion("12:01:00PM"));
+    }
+
+    @Test
+    public void testSimple02(){
+        assertEquals("00:01:00", timeConversion("12:01:00AM"));
+    }
+
+}
